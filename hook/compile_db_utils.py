7e7297438674a9d64f7b5d399f13c2383da8d157
mxmlnrdr <maxrieder@gmx.de>
2023-12-22 12:56:52 +0100

    add extraction for include directories from compile db

--- hook/compile_db_utils.py
+++ hook/compile_db_utils.py
@@ -20,3 +20,23 @@ def extract_source_file_list(compile_db_path: str) -> List[str]:
     for entry in data:
         file_list.append(entry["file"])
     return file_list
+
+
+def extract_include_directories(compile_db_path: str) -> List[str]:
+    with open(compile_db_path) as json_data:
+        data = json.load(json_data)
+        json_data.close()
+    entries = [entry.__str__() for entry in data]
+    entry_strings = [single.split(" ") for single in entries]
+    entry_strings_flat = [
+        item for sublist in entry_strings for item in sublist
+    ]
+    include_prefix = "-I"
+    include_directories = [
+        entry[len(include_prefix) :]
+        for entry in entry_strings_flat
+        if entry.startswith(include_prefix)
+    ]
+    # remove duplicates
+    include_directories = list(set(include_directories))
+    return include_directories
