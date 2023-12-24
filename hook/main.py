7a6e8d3f0ca4e0c000e527b2f96964d926a7778f
mxmlnrdr <maxrieder@gmx.de>
2023-12-24 15:35:25 +0100

    remove logging in main as not useful

--- hook/main.py
+++ hook/main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import logging
 import sys
 from typing import List
 
@@ -10,33 +9,25 @@ from hook.clang_tidy_runner import ClangTidyRunner
 
 
 def main(argv: List[str] = sys.argv) -> None:
-    logging.basicConfig(level=logging.INFO)
-    logging.debug("Input args: " + " ".join(argv))
-
     compile_db = find_compilation_database()
     if compile_db is None:
         sys.stderr.write(
             "No compilation database found. " "SKIPPING code checker run.\n"
         )
         sys.exit(0)
-    logging.debug("Compile db: " + compile_db.__str__())
 
     files_to_check = extract_file_pathes(argv[1:])
-    logging.debug("Input files: " + " ".join(files_to_check))
-
     compiled_files = get_compiled_files(
         files=files_to_check, compile_db=compile_db
     )
-    logging.debug("Compiled files to check: " + " ".join(compiled_files))
-
     if not compiled_files:
         sys.stderr.write(
             "No compiled files to check. " "SKIPPING code checker run.\n"
         )
         sys.exit(0)
+
     arguments = extract_arguments(argv)
     arguments.append("-p=" + compile_db.__str__())
-    logging.debug("Arguments: " + " ".join(arguments))
 
     cmd = ClangTidyRunner(arguments, compiled_files)
     return_code, output = cmd.run()
