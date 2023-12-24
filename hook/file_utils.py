1a807bcea03861e99cbf66796d8851f058daceb5
mxmlnrdr <maxrieder@gmx.de>
2023-12-24 16:34:43 +0100

    refactor file utils for compile db object

--- hook/file_utils.py
+++ hook/file_utils.py
@@ -1,9 +1,5 @@
 from typing import List
 
-from hook.compile_db_utils import (
-    extract_include_directories,
-    extract_source_file_list,
-)
 from hook.path_utils import (
     extract_header_pathes,
     match_absolute_headers_in_include_dirs,
@@ -12,16 +8,16 @@ from hook.path_utils import (
 )
 
 
-def get_compiled_files(files: List[str], compile_db: str) -> List[str]:
+def get_compiled_files(
+    files: List[str], include_dirs: List[str], source_files: List[str]
+) -> List[str]:
     relative_files = convert_to_relative_pathes(input_pathes=files)
 
     header_files = extract_header_pathes(files=relative_files)
-    include_dirs = extract_include_directories(compile_db_path=compile_db)
     compiled_files = match_absolute_headers_in_include_dirs(
         headers=header_files, include_dirs=include_dirs
     )
 
-    source_files = extract_source_file_list(compile_db_path=compile_db)
     compiled_files += match_absolute_pathes_based_on_relative_pathes(
         absolute_pathes=source_files, relative_pathes=relative_files
     )
